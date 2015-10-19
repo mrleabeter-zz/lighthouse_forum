af3911d54104ca233aeca158722c53f708a15de2
Christopher Leabeter <christopherleabeter@gmail.com>
2015-10-19 17:44:56 -0400

    Completed step 9: adding edit post functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ LighthouseForum::Application.routes.draw do
 
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  get 'posts/:id/edit', to: 'posts#edit'
+  patch 'posts/:id', to: 'posts#update'
   post 'posts', to: 'posts#create'
+
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
